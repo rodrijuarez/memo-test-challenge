175b90d691194aa5de2b5c3acf0d3ae5b5975c5d
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2023-05-04 17:38:12 -0300

    Design changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,19 +5,19 @@ const Card = ({ card, index, onClick, isFlipped }) => {
   return (
     <ReactCardFlip key={`${card.id}-${index}`} isFlipped={isFlipped}>
       <div
-        className={`aspect-square bg-white w-full m-w-fit h-full rounded shadow p-4 ${
+        className={`aspect-square bg-emerald-200 hover:bg-emerald-600 w-full m-w-fit h-full rounded shadow p-4 ${
           isFlipped ? '' : 'cursor-pointer'
         }`}
         onClick={() => !isFlipped && onClick(index)}
       >
-        <div className="bg-black w-full h-full">
+        <div className="bg-black hover:bg-emerald-600 w-full h-full">
           <h2 className="text-2xl text-center -translate-y-1/2 relative inset-y-1/2 text-white">
             {index + 1}
           </h2>
         </div>
       </div>
       <div
-        className={`aspect-square bg-white w-full h-full rounded shadow p-4 ${
+        className={`aspect-square bg-emerald-200 w-full h-full rounded shadow p-4 ${
           isFlipped ? '' : 'cursor-pointer'
         }`}
         onClick={() => !isFlipped && onClick(index)}
