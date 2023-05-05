d40098e0afbf2dbac99c54e3c30c27af5eb6f0a4
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2023-05-05 11:38:09 -0300

    Jest config

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,23 @@ module.exports = {
     '\\.(css|less|scss|sss|styl)$': 'identity-obj-proxy',
   },
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      {
+        presets: [
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          '@babel/preset-typescript',
+          '@babel/preset-react',
+        ],
+        plugins: [
+          // Add your Jest-specific Babel plugins here.
+        ],
+      },
+    ],
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   transformIgnorePatterns: [
     '/node_modules/',
     '^.+\\.module\\.(css|sass|scss)$',
   ],
-};
+}
